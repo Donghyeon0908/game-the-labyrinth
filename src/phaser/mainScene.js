4df52c8f130009062a6dc538bd55776d903a2372
DonghyeonLee91 <donghyeonlee0908@gmail.com>
2022-11-16 20:35:01 +0900

    feat: 캐릭터움직임
    1. 캐릭터 움직임 구현

--- src/phaser/mainScene.js
+++ src/phaser/mainScene.js
@@ -1,6 +1,7 @@
-import Phaser from "phaser";
+import { Scene, Math as pMath } from "phaser";
 
-export default class MainScene extends Phaser.Scene {
+const { Vector2 } = pMath;
+export default class MainScene extends Scene {
   constructor() {
     super("MainScene");
   }
@@ -16,15 +17,15 @@ export default class MainScene extends Phaser.Scene {
   create() {
     const mapDataArray = [
       [2, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-      [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-      [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
       [0, 1, 1, 0, 0, 0, 1, 1, 1, 0],
-      [0, 0, 1, 0, 0, 1, 0, 0, 1, 1],
-      [0, 0, 1, 0, 0, 0, 0, 0, 1, 1],
+      [0, 0, 1, 0, 0, 1, 1, 0, 1, 1],
+      [0, 0, 1, 1, 0, 0, 1, 0, 1, 1],
       [0, 0, 1, 1, 1, 1, 1, 1, 1, 0],
-      [0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 1, 3],
+      [0, 1, 0, 1, 0, 0, 0, 1, 1, 1],
+      [0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+      [0, 0, 0, 1, 1, 0, 0, 1, 1, 3],
     ];
 
     const map = this.make.tilemap({
@@ -34,8 +35,50 @@ export default class MainScene extends Phaser.Scene {
     });
     map.addTilesetImage("tiles");
 
+    const startingPoint = map.findTile((tile) => tile.index === 3);
     const layer = map.createLayer(0, "tiles", 0, 0);
 
-    this.player = this.physics.add.sprite(50, 50, "player");
+    this.player = this.physics.add.sprite(
+      startingPoint.pixelX + 30,
+      startingPoint.pixelY + 30,
+      "player"
+    );
+
+    this.target = new Vector2();
+    this.input.on("pointerup", (pointer) => {
+      const { worldX, worldY } = pointer;
+
+      this.target.x = worldX;
+      this.target.y = worldY;
+      this.physics.moveToObject(this.player, this.target, 200);
+    });
+
+    this.physics.add.collider(this.player, layer);
+    layer.setCollision(0);
+
+    this.anims.create({
+      key: "walk",
+      frames: this.anims.generateFrameNumbers("player", {
+        frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      }),
+      frameRate: 5,
+      repeat: -1,
+    });
+    this.player.play("walk");
+  }
+
+  update() {
+    if (this.player.body.speed > 0) {
+      const mousePoint = pMath.Distance.Between(
+        this.player.x,
+        this.player.y,
+        this.target.x,
+        this.target.y
+      );
+
+      if (mousePoint < 4) {
+        this.player.body.reset(this.target.x, this.target.y);
+      }
+    }
   }
 }
