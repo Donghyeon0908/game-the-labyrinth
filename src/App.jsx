3e5c3988bf916c93b79f6d0d0637d55272195e54
DonghyeonLee91 <donghyeonlee0908@gmail.com>
2023-03-30 14:40:14 +0900

    refactor: 모바일 접속 차단

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import GameStartingPage from "./components/GameStartingPage";
@@ -9,6 +9,13 @@ import NotFoundPage from "./components/NotFoundPage";
 
 function App() {
   GlobalStyle();
+
+  useEffect(() => {
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+      alert("이 서비스는 모바일에서 이용할 수 없습니다. PC에서 이용해주세요.");
+      window.location.href = "about:blank";
+    }
+  }, []);
 
   return (
     <>
